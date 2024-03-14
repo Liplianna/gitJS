af3c6257794130e6caefef964d783f55644e45e7
Hanna Liplianin <h.liplianin@godeltech.com>
2024-03-14 06:19:09 -0700

    Variables created, Mi deleted

---
+++ variablesHanna.js
@@ -0,0 +1,14 @@
+let greeting "Hello!";
+console.log(greeting);
+
+let message;
+message = "any message";
+console.log(message);
+
+let number = 1;
+number = 2;
+console.log(number);
+
+const PI = 3.14;
+PI = 4;
+console.log(Pi);
